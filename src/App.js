edc207882c75574efcb2ce0c6c5a47c0c8169642
Samuel-Muller <Sam.C.Muller@Outlook.com>
2021-04-29 16:10:40 +0000

    Add schedule view page

--- src/App.js
+++ src/App.js
@@ -78,6 +78,11 @@ function App(props) {
                     Form a group
                   </Link>
                 </li>
+                <li>
+                  <Link class="login" to="/list">
+                    My schedule
+                  </Link>
+                </li>
               </ul>
             </nav>
 
@@ -90,6 +95,10 @@ function App(props) {
 
               <Route path="/grouping">
                 <Grouping />
+              </Route>
+              
+              <Route path="/list">
+                <List />
               </Route>
 
               <Route path="/">
@@ -181,6 +190,33 @@ function Add(props) {
     </form>
   );
 }
+
+function List() {
+  return (
+    <div class = "schedule">
+      <div>Your Schedule</div>
+      
+      <h5>Sunday</h5>
+      <h5>Monday</h5>
+        <div>CS301 002 12:30 pm 1:50pm</div>
+        <div>YWCC307 012 2:30 pm 3:50pm</div>
+        <div>ACCT117 102 6:00 pm 8:50pm</div>
+      <h5>Tuesday</h5>
+        <div>CS490 006 12:30 pm 1:50pm</div>
+        <div>CS435 006 2:30 pm 3:30pm</div>
+        <div>CS435 006 4:00 pm 5:20pm</div>
+      <h5>Wednesday</h5>
+        <div>CS301 002 12:30 pm 1:50pm</div>
+      <h5>Thursday</h5>
+        <div>CS490 006 12:30 pm 1:50pm</div>
+        <div>CS435 006 4:00 pm 5:20pm</div>
+        <div>CS351 102 6:00 pm 8:50pm</div>
+      <h5>Friday</h5>
+      <h5>Saturday</h5>
+    </div>
+  );
+}
+
 App.propTypes = {
   name: PropTypes.string,
   email: PropTypes.string,
