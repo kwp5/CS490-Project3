f7495fce8dc87bc2f94e174d8e220d69b6fc9165
Samuel-Muller <Sam.C.Muller@Outlook.com>
2021-05-05 02:35:39 +0000

    fix day submission on add course form

--- src/App.js
+++ src/App.js
@@ -180,25 +180,25 @@ function Add(props) {
       </div>
 
       <div>
-        <input type="radio" id="dayChoice0" name="day" value="0" defaultChecked="checked" onChange={event => setDay(event.target.value)}/>
+        <input type="radio" id="dayChoice0" name="day" value="0" onChange={event => setDay(event.target.value)}/>
         <label htmlFor="dayChoice0">Sunday</label>
 
-        <input type="radio" id="dayChoice1" name="day" value="1" />
+        <input type="radio" id="dayChoice1" name="day" value="1" onChange={event => setDay(event.target.value)}/>
         <label htmlFor="dayChoice1">Monday</label>
 
-        <input type="radio" id="dayChoice2" name="day" value="2" />
+        <input type="radio" id="dayChoice2" name="day" value="2" onChange={event => setDay(event.target.value)}/>
         <label htmlFor="dayChoice2">Tuesday</label>
 
-        <input type="radio" id="dayChoice3" name="day" value="3" />
+        <input type="radio" id="dayChoice3" name="day" value="3" onChange={event => setDay(event.target.value)}/>
         <label htmlFor="dayChoice3">Wednesday</label>
 
-        <input type="radio" id="dayChoice4" name="day" value="4" />
+        <input type="radio" id="dayChoice4" name="day" value="4" onChange={event => setDay(event.target.value)}/>
         <label htmlFor="dayChoice4">Thursday</label>
 
-        <input type="radio" id="dayChoice5" name="day" value="5" />
+        <input type="radio" id="dayChoice5" name="day" value="5" onChange={event => setDay(event.target.value)}/>
         <label htmlFor="dayChoice5">Friday</label>
 
-        <input type="radio" id="dayChoice6" name="day" value="6" />
+        <input type="radio" id="dayChoice6" name="day" value="6" onChange={event => setDay(event.target.value)}/>
         <label htmlFor="dayChoice6">Saturday</label>
       </div>
 
